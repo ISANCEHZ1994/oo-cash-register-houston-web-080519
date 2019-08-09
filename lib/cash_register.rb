9aa6e8af35117499791fab5a747bba10f866eb52
Israel Sanchez <israel.pro1994@gmail.com>
2019-08-09 14:04:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
-
+class CashRegister
+  attr_accessor :total, :discount, :preTotal
+  
+  def initialize (discount = 0)
+    @discount = discount
+    @total = 0
+  end
+  
+  def apply_discount
+    @total_discount -= (@total * @discount)/100
+  end
+  
+  def add_item(title,price)
+    @total += 
+  end
+  
+  def apply_discount
+  end
+  
+  def void_last_transaction()
+  end 
+  
+end
